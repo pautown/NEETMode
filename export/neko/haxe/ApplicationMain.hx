78dba9a0ea62a0b78c56d07ccea719a11de95346
Paul Town <paul@town.com>
2019-07-04 20:23:03 -0400

    Set up activity array and creation logic as well as a persistent array of activities.

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "13";
+		app.meta["build"] = "2";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "NEETMode";
 		app.meta["name"] = "NEETMode";
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,6 +10,7 @@ import flixel.math.FlxAngle;
 import flixel.math.FlxMath;
 import flixel.util.FlxSave;
 import flixel.tweens.FlxTween;
+import flixel.ui.FlxButton;
 
 using flixel.util.FlxSpriteUtil;
 
@@ -18,16 +19,32 @@ class PlayState extends FlxState
 
 	public var input_text:FlxInputText = new FlxInputText(10,10,Std.int(FlxG.width/1.3),"hehehe", 20);
 	public var input_text_label = new flixel.text.FlxText(0,0, 0, "Title of task", 20);
+	public var new_activity_label = new flixel.text.FlxText(0,0, 0, "Title of task", 20);
 	public var new_activity_step_array:Array<String> = ["Enter Activity Name: ", "Enter Activity Description:", "Enter Activity Type, (t)ime or (r)ange: ", "Enter Activity Unit of Measure, eg 'minutes' or 'pages': ", "Enter Activity Min Amount: ", "Enter Activity Max Amount: ", "Enter Average Frequency Per Week (1-7) Activity Occurs: "];
 	public var program_state:String = "new_activity";
 	public var new_activity_selector:Int = 0;
 	var touch_start_flx_object:flixel.FlxObject = new flixel.FlxObject(0,0,0,0);
 	var touch_start_flx_sprite:FlxSprite = new FlxSprite(0,0);
-	var game_save:FlxSave = new FlxSave();
+	var activity_save:FlxSave = new FlxSave();
+	var new_activity_next_button:FlxButton;
+	var new_activity_previous_button:FlxButton;
+	var new_activity_create_button:FlxButton;
+	var total_activities_array:Array<Array<String>>;
+	var new_activity_array:Array<String>;
 
 	override public function create():Void
 	{
 		super.create();
+		FlxG.mouse.useSystemCursor = true;
+		activity_save.bind("NEETMode");
+		if (activity_save.data.total_activities_array == null){
+			activity_save.data.total_activities_array = []; 
+			activity_save.flush();
+		}
+		total_activities_array = activity_save.data.total_activities_array;
+		new_activity_next_button = new FlxButton(50, 50, "Next", click_new_activity_next_btn);
+		new_activity_previous_button = new FlxButton(50, 50, "Previous", click_new_activity_previous_btn);
+		new_activity_create_button = new FlxButton(50, 50, "Create", click_new_activity_create_btn);
 		add_create_new_activity_screen();
 
 	}
@@ -68,10 +85,8 @@ class PlayState extends FlxState
 		
 
 		if(program_state == "new_activity"){
-			if(input_left) new_activity_selector --;
-			else if(input_right) new_activity_selector ++;
-			if(new_activity_selector < 0) new_activity_selector = new_activity_step_array.length - 1;
-			else if(new_activity_selector > new_activity_step_array.length - 1) new_activity_selector = 0;
+			//if(input_left) new_activity_selector --;
+			if(FlxG.keys.justPressed.ENTER) click_new_activity_next_btn();
 			if(input_any) input_text_label.text = new_activity_step_array[new_activity_selector] + " ("+ Std.string(new_activity_selector + 1) + "/" + Std.string(new_activity_step_array.length) + ")";
 			/*if(new_activity_step_array[new_activity_selector] == "Tiny") grid_magnitude = 3;
 			else if(new_activity_step_array[new_activity_selector] == "2x2") grid_magnitude = 2;
@@ -81,11 +96,56 @@ class PlayState extends FlxState
 		
 		}
 	}
-
+	public function click_new_activity_next_btn():Void
+	{
+		trace("next");
+		new_activity_array[new_activity_selector] = input_text.text;
+		new_activity_selector ++;
+		update_new_activity_label();
+		if(new_activity_selector > new_activity_step_array.length - 1) new_activity_selector = 0;	
+		input_text.text = new_activity_array[new_activity_selector];
+	}
+	public function click_new_activity_previous_btn():Void
+	{
+		trace("previous");
+		new_activity_array[new_activity_selector] = input_text.text;
+		new_activity_selector --;
+		update_new_activity_label();
+		if(new_activity_selector < 0) new_activity_selector = new_activity_step_array.length - 1;
+		input_text.text = new_activity_array[new_activity_selector];
+	}
+	public function click_new_activity_create_btn():Void
+	{
+		trace("create");
+		total_activities_array.push(new_activity_array.copy());
+		new_activity_array = ["???","???","???","???","???","???","???"];
+		update_new_activity_label();
+	}
+	public function update_new_activity_label():Void
+	{
+		new_activity_label.text = "Activity Name: " + new_activity_array[0] + "\n"
+								+ "Activity Description: " + new_activity_array[1] + "\n"
+								+ "Activity Type: " + new_activity_array[2] + "\n"
+								+ "Activity Unit of Measure: " + new_activity_array[3] + "\n"
+								+ "Activity Min: " + new_activity_array[4] + "\n"
+								+ "Activity Max: " + new_activity_array[5] + "\n"
+								+ "Activity Frequency: " + new_activity_array[6]
+								+ "Total Activities: " + Std.string(total_activities_array.length);
+	}
 	public function add_create_new_activity_screen():Void
 	{
+		new_activity_array = ["???","???","???","???","???","???","???"];
 		add(input_text_label);
 		add(input_text);
+		add(new_activity_next_button);
+		add(new_activity_previous_button);
+		add(new_activity_create_button);
+		add(new_activity_label);
 		input_text.y = input_text_label.y + input_text_label.height;
+		new_activity_next_button.y = new_activity_previous_button.y = input_text.y + input_text.height + 10;
+		new_activity_label.y = new_activity_next_button.y + new_activity_next_button.height + 10;
+		new_activity_next_button.x = input_text.x + input_text.width - new_activity_next_button.width - 10;
+		new_activity_previous_button.x = input_text.x + 10;
+		new_activity_create_button.y = FlxG.height - new_activity_create_button.height - 10;
 	}
 }
