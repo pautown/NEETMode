cb43e68fd86a5a27b678509cb4b9f4ca66e0c791
Paul Town <paul@town.com>
2019-10-06 00:48:21 -0400

    Update HTML build.

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "30";
+		app.meta["build"] = "32";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "NEETMode";
 		app.meta["name"] = "NEETMode";
